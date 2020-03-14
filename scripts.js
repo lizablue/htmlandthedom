e6596014b2ee4133881fb8ea9a4873e6cde77009
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-03-14 08:44:48 -0500

    color changing div

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // create first button
     let btn = document.createElement('button');
     let btnText = document.createTextNode('click me');
     btn.style.display = "block";
     btn.appendChild(btnText);
     document.body.prepend(btn);
 
+    // display alert on button click
     btn.addEventListener('click', function() {
         let quoteArray = [
             '\"Would I rather be feared or loved? Easy- both. I want people to be afriad of how much they love me.\" - Michael Scott',
@@ -17,11 +19,21 @@ document.addEventListener('DOMContentLoaded', function() {
         alert(randomQuote);
     })
 
+    // new button with text box
     let btnInput = document.getElementById('btnInput');
 
+    // on click display what was text box input
     btnInput.addEventListener('click', function() {
         let str = document.getElementById('txtInput').value;
         alert(str);
     })
 
+    // change div bg color on hover
+    let divColor = document.getElementById('color');
+    divColor.addEventListener('mouseover', function() {
+        divColor.style.backgroundColor = 'blue';
+    })
+    divColor.addEventListener('mouseout', function(){
+        divColor.style.backgroundColor = null;
+    })
 })
